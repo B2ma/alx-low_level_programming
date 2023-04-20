7173f8e3d2faab5f22519a8e815fad584e54f4d3
B2ma <beatrice.terer@gmail.com>
2023-04-20 05:53:47 -0700

    Created  6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+  * main -Enthy point
+  * Print f prints Size of Char in bytes
+  * Print f prints Size of int in bytes
+  * Print f prints Size of long int in bytes
+  * Print f prints Size of long long int in bytes
+  * Print f prints Size of float in bytes
+  */
+int main(void)
+{
+	printf("Size of Char in bytes: 1%d bytes\n", sizeof(char));
+	printf("Size of int in bytes: 1%d bytes\n", sizeof(char));
+	Printf("Size of long int in bytes: 1%d bytes\n", sizeof(char));
+	printf("Size of long long int in bytes: 1%d bytes\n", sizeof(char));
+	printf("Size of float in bytes: 1%d bytes\n", sizeof(char));
+	return (0);
+}
