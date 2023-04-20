0641c9a7f4af9db170576a70a3bf1517cfee7544
B2ma <beatrice.terer@gmail.com>
2023-04-20 04:34:24 -0700

    Corrected 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
-  *C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
-  *
+  * it prints exactly "Programming is like building a multilingual puzzle,followed by a new line
   */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
