f7de74401d157f36a0c2bb58406122b2eaad5b0e
B2ma <beatrice.terer@gmail.com>
2023-04-20 05:00:44 -0700

    Corrected 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
   * printf prints the contents in parenthesis
   *
   * Return : Always 0 (Success)
-  **/
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
