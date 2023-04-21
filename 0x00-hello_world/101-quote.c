4119faeb7d61614dca60b924d99443c934f4bd2c
B2ma <beatrice.terer@gmail.com>
2023-04-21 10:25:35 +0000

    amended 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,10 @@
   */
 int main(void)
 {
-	const char* message = "and that piece of art is useful\n";
+	const char *message = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, message, 31);
+	write(STDERR_FILENO, message, 59);
 	return (1);
 }
 
