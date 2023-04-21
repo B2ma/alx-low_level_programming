cc4965c85801c2e930ea8bbb4807f20ee5bac854
B2ma <beatrice.terer@gmail.com>
2023-04-21 11:54:42 +0000

    amended 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,13 @@
   * main - Entry point
   * Description: prints and that piece of art is useful"
   *	- Dora Korpar, 2015-10-19 to standard error
-  * Return: 1 error message
+  * Return: Always 0 (Success)
   */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - "
-		"Dora Korpar, 2015-10-19\n";
+	char const *message = "and that piece of art is useful\" - Dora Korpar, "
+										"2015-10-19\n";
 
 	write(STDERR_FILENO, message, 59);
-	return (1);
+	return (0);
 }
-
