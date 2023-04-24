841c73ffef9bcd057cd1653b89278acda21a03cf
B2ma <beatrice.terer@gmail.com>
2023-04-24 07:44:17 -0700

    Created 4-print_alphabt.c and its script 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+  * main - Entry point
+  * Description: put char prints the alphabet in lowercase except q and e
+  * Return 0 always (Success)
+  */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+	if (alphabet != 'q' && alphabet != 'e')
+	{
+		putchar(alphabet);
+	}
+	alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
