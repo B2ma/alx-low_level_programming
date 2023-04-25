a44295401a5adf9234af9797fbc1cc8237f2c551
B2ma <beatrice.terer@gmail.com>
2023-04-25 13:12:17 -0700

    Created 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,36 @@
 #include <stdio.h>
 
 /**
-  * print_to_98 prints all natural numbers from n to 9
+  * print_to_98 - prints all natural numbers from n to 9
   *	followed by a new line
+  * @n: the starting number
   * Return: always 0
   */
 void print_to_98(int n)
 {
-	int num;
+	int i;
 
-	for (num >= n; num <= 9; num++)
+	if (n <= 98)
 	{
-		putchar(num);
+	for (i = n; i < 98; i++)
+	{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		putchar(',');
+		putchar(' ');
 	}
+	}
+	else
+	{
+	for (i = 1; i > 98; i++)
+	{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('9');
+	putchar('8');
 	putchar('\n');
-	return (0);
-
 }
