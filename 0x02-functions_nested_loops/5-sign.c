a9122ccbe6d2fb53143ba7685bfd88ea852041c2
B2ma <beatrice.terer@gmail.com>
2023-04-25 13:52:00 -0700

    amended 5-sign.c to align with bettys documentation style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_sign prints the sign of a nuber
+  * print_sign - prints the sign of a nuber
   * @num: number to check
   * Return: 1 and prints + if num is greater than zero
   *	0 and prints 0 if num is zero
