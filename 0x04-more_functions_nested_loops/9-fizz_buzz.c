a40734fe9ac44a064f3f2a63a8410b8fbc7e68d7
B2ma <beatrice.terer@gmail.com>
2023-04-27 13:57:15 -0700

    amended 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,46 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-  * main - Entry point
-  * Return: 0 success
-  */
+ * main - Entry point
+ * Return: 0 success
+ */
 int main(void)
 {
 	int i = 1;
+	int chars_printed = 0;
 
 	while (i <= 100)
 	{
+		int chars_to_print;
+
 		if (i % 3 == 0 && i % 5 == 0)
 		{
+			chars_to_print = 8;
 			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
+			chars_to_print = 5;
 			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
+			chars_to_print = 5;
 			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", i);
+			chars_to_print = printf("%d ", i);
+		}
+		chars_printed += chars_to_print;
+		if (chars_printed >= 143)
+		{
+			putchar('\n');
+			chars_printed = 0;
 		}
 		i++;
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
+
