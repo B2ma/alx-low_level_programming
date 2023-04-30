42736579bd9aa36a46652a5d0c6fee839c03cf8f
B2ma <beatrice.terer@gmail.com>
2023-04-30 19:57:33 +0000

    Amended 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,46 @@
-#include<stdio.h>
+#include <stdio.h>
+
+#define MAX_DIGITS 30
 
 /**
-  * main - Entry point
-  * Description: prints the first 98 Fibonacci numbers
-  *	starting with 1 and 2
-  * Return: 0
-  */
-int main(void)
+ * print_fibonacci - prints the first 98 Fibonacci numbers
+ */
+void print_fibonacci(void)
 {
-	int n = 98;
-	unsigned long int j1 = 1, j2 = 2, nextnum;
+	unsigned long f1 = 1, f2 = 2, tmp, f1o = 0, f2o = 0, tmpo = 0;
+	int i;
 
-	printf("1, 2,");
-	for (n = 3; n <= 98; n++)
+	printf("%lu, %lu", f1, f2);
+	for (i = 3; i <= 98; i++)
 	{
-		nextnum = j1 + j2;
-		j1 = j2;
-		j2 = nextnum;
-		if (n != 98)
+		tmp = f1 + f2;
+		tmpo = f1o + f2o + (tmp < f1 || tmp < f2);
+		f1 = f2;
+		f1o = f2o;
+		f2 = tmp;
+		f2o = tmpo;
+		if (f1o > 0)
 		{
-			printf(", %lu", nextnum);
+			printf("%0*lu%0*lu", MAX_DIGITS - 1, f1o, MAX_DIGITS, f1);
 		}
 		else
 		{
-			printf(", %lu\n", nextnum);
+			printf("%lu", f1);
+		}
+		if (i < 98)
+		{
+			printf(", ");
 		}
 	}
+	printf("\n");
+}
+
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	print_fibonacci();
 	return (0);
 }
