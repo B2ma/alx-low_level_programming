271b13142236789edd279fdf0335b6f33f1cc05a
B2ma <beatrice.terer@gmail.com>
2023-04-30 13:25:50 +0000

    Amended 100-time_tables.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,19 +6,15 @@
   */
 void print_times_table(int n)
 {
-	int i, j;
-	int times;
+	int i, j, times;
 
-	if (n <= 0 || n > 15)
+	if (n >= 0 && n <= 15)
 	{
-		return;
-	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			times = i * j;
-
 			if (j == 0)
 			{
 				_putchar('0');
@@ -27,19 +23,16 @@ void print_times_table(int n)
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(' ');
 				_putchar(times + '0');
 			}
 			else if (times < 100)
 			{
-				_putchar(' ');
 				_putchar(' ');
 				_putchar((times / 10) + '0');
 				_putchar((times % 10) + '0');
 			}
 			else
 			{
-				_putchar(' ');
 				_putchar((times / 100) + '0');
 				_putchar(((times / 10) % 10) + '0');
 				_putchar((times % 10) + '0');
@@ -47,8 +40,10 @@ void print_times_table(int n)
 			if (j != n)
 			{
 				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
+	}
 	}
 }
