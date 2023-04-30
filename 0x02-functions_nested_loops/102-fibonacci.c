b5c5f8e506d79c7cc74d3dba0f530cf92cdad1b9
B2ma <beatrice.terer@gmail.com>
2023-04-30 15:49:19 +0000

    Created betty 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+  * main - Entry point
+  * Description: prints the first 50 Fibonacci numbers
+  * Return: o
+  */
+int main(void)
+{
+	int i, n = 50;
+	unsigned long ji = 1, j2 = 2, nextnum;
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%ld ,", nextnum);
+		nextnum = j1 + j2;
+		ji = j2;
+		j2 = nextnum;
+	}
+	printf("\n");
+	return (0);
+}
