ce8215da13f8f85a2db4b88124f57f3feab223d4
B2ma <beatrice.terer@gmail.com>
2023-04-30 04:35:39 -0700

    Created im 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+  * print_times_table - prints the n times table
+  * @n: the number
+  */
+void print_times_table(int n)
+{
+	int i, j;
+	int times = i * j;
+
+	if (n <= 0 || n > 15)
+	{
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (times < 10)
+			{
+				_putchar('\t');
+				_putchar(times + '0');
+			}
+			else if (times < 100)
+			{
+				_putchar('\t');
+				_putchar(times / 10 + '0');
+				_putchar(times % 10 + '0');
+			}
+			else
+			{
+				_putchar('\t');
+				_putchar(times / 100 + '0');
+				_putchar(times % 100 + '0');
+				_putchar(times % 10 + '0');
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar('\t');
+			}
+		}
+		_putchar('\n');
+	}
+}
