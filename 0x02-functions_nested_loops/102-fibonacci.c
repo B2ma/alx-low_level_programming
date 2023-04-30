9e5908e3e04eb5ab6d7c64b248e2deaf4dbbcc46
B2ma <beatrice.terer@gmail.com>
2023-04-30 16:43:59 +0000

    Amended 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,19 @@
 int main(void)
 {
 	int i, n = 50;
-	unsigned long j1 = 1, j2 = 2, nextnum;
+	unsigned long j1 = 1, j2 = 1, nextnum = 1;
 
 	for (i = 1; i <= n; i++)
 	{
-		printf("%ld ,", nextnum);
+		printf("%ld", nextnum);
 		nextnum = j1 + j2;
 		j1 = j2;
 		j2 = nextnum;
+		if (i != n)
+		{
+			printf(", ");
+		}
+		
 	}
 	printf("\n");
 	return (0);
