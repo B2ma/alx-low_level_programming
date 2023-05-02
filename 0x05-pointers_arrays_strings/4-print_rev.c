cf7b229082cba25fb0c625ee733d8393217cb812
B2ma <beatrice.terer@gmail.com>
2023-05-02 17:41:56 +0000

    Amended 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,5 +3,19 @@
 /**
   * print_rev - prints a string, in reverse
   *	followed by a new line
+  * @s: string to be printed
   */
 void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 0; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
