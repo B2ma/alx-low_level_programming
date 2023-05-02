ee1bb8a4124dd54c253e4b53fce94cc96ef1bdfd
B2ma <beatrice.terer@gmail.com>
2023-05-02 17:21:09 +0000

    Amended ./2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
 /**
-  * int _strlen - returns the length of a string
+  * _strlen - returns the length of a string
+  * @s: the string
   * Return: strlen
   */
 int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+	length++;
+	return (length);
+}
