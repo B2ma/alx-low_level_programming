1527219e9ff9b81a111c04bf08a819c4e68583f1
B2ma <beatrice.terer@gmail.com>
2023-05-02 17:29:41 +0000

    Amended main.h and 3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,5 +2,13 @@
 
 /**
   * _puts - rints a string, followed by a new line, to stdout
+  * @str: the string to print
   */
 void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
