4e571a697c472c51b96002c2d592c65923b79034
B2ma <beatrice.terer@gmail.com>
2023-05-03 11:24:38 +0000

    Created 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * _atoi - converts a string to an integer
+  * @s: the pointer to convert
+  * Return: integer
+  */
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int n = 0;
+	int min = 0;
+	int isi = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 45)
+		{
+			min *= -1;
+		}
+		while (s[i] >= 48 &&s[i] <= 57)
+		{
+			isi *= 1;
+			n = (n * 10) + (s[i] - '0');
+			i++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		 i++;
+		}
+	n *= min;
+	return (n);
+}
