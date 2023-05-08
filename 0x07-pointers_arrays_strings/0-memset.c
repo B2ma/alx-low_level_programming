0a727dffc6d1dc12aac0fbef9ccf69148c605575
B2ma <beatrice.terer@gmail.com>
2023-05-08 08:46:04 +0000

    Amended 0-main.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,14 @@
   * @n:bytes of the memory area
   * Return: pointer to the memory area s
   */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
+	int m = 0;
+
+	for (; n > 0; m++)
+	{
+		s[m] = b;
+		n--;
+	}
 	return (s);
 }
