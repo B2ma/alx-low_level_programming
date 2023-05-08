d1f35536edd9efcf164005497d738e4f371f7673
B2ma <beatrice.terer@gmail.com>
2023-05-08 07:22:36 +0000

    Created 0-memset.c and main.h files and necessary main.c filee

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _memset - fills memory with a constant byte
+  * @s:pointer to the memory area
+  * @b: the constant byte
+  * @n:bytes of the memory area
+  * Return: pointer to the memory area s
+  */
+char *_memset(char *s, char b, unsigned int n);
+{
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ 0x07-pointers_arrays_strings is a project that involved pointer to a pointer.
