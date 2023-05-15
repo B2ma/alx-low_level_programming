e279730ddceb1da2f49f99555bb4d98f5d42863c
B2ma <beatrice.terer@gmail.com>
2023-05-15 12:45:47 +0000

    Created 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * str_concat - Concatinates two strings
+  * @s1: first string
+  * @s2: secong string
+  * Return: NULL on failure or new pointer with s1 & s2 contents
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int s1Len;
+	int s2Len;
+	char *s1s2Malloc;
+
+	s1Len = strlen(s1);
+	s2Len = strlen(s2);
+	s1s2Malloc = malloc(sizeof(char) * (s1Len + s2Len + 1));
+	if (s1s2Malloc == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		strcpy(s1s2Malloc, s1);
+		strcat(s1s2Malloc, s2);
+	}
+	return (s1s2Malloc);
+}
