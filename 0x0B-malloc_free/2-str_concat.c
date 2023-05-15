7d0e93555dee62e69cf9d393112e2156ddd87793
B2ma <beatrice.terer@gmail.com>
2023-05-15 13:18:10 +0000

    Ammended 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,8 @@ char *str_concat(char *s1, char *s2)
 	int s2Len = 0;
 	char *s1s2Malloc = NULL
 
-	if (s1 != NULL)
-		s1Len = strlen(s1);
-	if (s2 != NULL)
-		s2Len = strlen(s2);
+	s1Len = strlen(s1);
+	s2Len = strlen(s2);
 	s1s2Malloc = malloc(sizeof(char) * (s1Len + s2Len + 1));
 	if (s1s2Malloc == NULL)
 	{
