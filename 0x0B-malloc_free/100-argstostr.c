b3ae84789846e5fddc56b950fa8353a8af39341b
B2ma <beatrice.terer@gmail.com>
2023-05-16 13:55:18 +0000

    Created 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * argstostr - concatinates all the arguments of program
+  * @ac: Number of arguments
+  * @av: pointer to the array of strings
+  * Return: pointer to strings, NULL otherwise
+  */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int tStringLength = 0;
+	char *avMalloc;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		tStringLength += strlen(av[i]);
+	}
+	avMalloc = (char *)malloc(sizeof(char) * (tStringLength + 1));
+	if (avMalloc == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		strcat(avMalloc, av[i]);
+		strcat(avMalloc, "\n");
+	}
+	return (avMalloc);
+}
