7f9893f4b0b4b546df8700a0cbd0378d36a0a473
B2ma <beatrice.terer@gmail.com>
2023-05-22 09:41:28 +0000

    Created README.md,dog.h in 0x0E-structures_typedef directory

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+This project is about Structures and typedef whose objective are to achieve;
+-What are structures, when, why and how to use them
+-How to use typedef
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+  * struct dog - my created structure
+  * @name: name details
+  * @age: Age details
+  * @owner: owner details
+  * Destription: this give the details of a particular dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
