6d9e458ca76f28b52550802dd78362b76d0827f2
B2ma <beatrice.terer@gmail.com>
2023-05-22 11:04:23 +0000

    Amended 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("%s\n", d->name != NULL ? d->name : "(nil)");
+	printf("%s\n", d->name != NULL ? d->name : "Name: :(nil)");
 	printf("%f\n", d->age);
 	printf("%s\n", d->owner != NULL ? d->owner : "(nil)");
 }
