c7faf9088eeaadca3910aae3ef569810c658e2dc
B2ma <beatrice.terer@gmail.com>
2023-05-26 16:17:39 +0000

    Amended 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,10 +3,11 @@
 
 /**
   * main - Entry point
-  * Desricption: prints the minimum number of coins
+  * Description: prints the minimum number of coins
   *	to make change for an amount of money
   * @argc: number of arguments provided in the program
   * @argv: array of pointers to the strings
+  * Return: number of coins
   */
 int main(int argc, char **argv)
 {
