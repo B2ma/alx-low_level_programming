d7f86986a87afc189db23ae14ee6e954b7e5ea69
B2ma <beatrice.terer@gmail.com>
2023-05-26 16:10:36 +0000

    Created 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,4 +7,32 @@
   *	to make change for an amount of money
   * @argc: number of arguments provided in the program
   * @argv: array of pointers to the strings
+  */
+int main(int argc, char **argv)
+{
+	int n;
+	int coins;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n <= 0)
+	{
+		printf("0\n");
+	}
+	coins = 0;
+	coins += n / 25;
+	n %= 25;
+	coins += n / 10;
+	n %= 10;
+	coins += n / 5;
+	n %= 5;
+	coins += n / 2;
+	n %= 2;
+	coins += n;
+	printf("%d\n", coins);
+	return (0);
+}
