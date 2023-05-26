64e6e506f8d6571a78f4570fb8a232a24307279d
B2ma <beatrice.terer@gmail.com>
2023-05-26 15:00:15 +0000

    Amended 3-mul.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+  * main - Entry point
+  * Desricption: prints the minimum number of coins
+  *	to make change for an amount of money
+  * @argc: number of arguments provided in the program
+  * @argv: array of pointers to the strings
+
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,18 @@
  */
 int main(int argc, char **argv)
 {
-	int i;
+	int n1, n2;
 	int mult = 1;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		fprintf(stderr, "Error\n");
 		return (1);
 	}
-	for (i = 1; i <= argc - 1; i++)
-	{
-		mult *= atoi(argv[i]);
-	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	mult = n1 * n2;
+	
 	printf("%d\n", mult);
 	return (0);
 }
