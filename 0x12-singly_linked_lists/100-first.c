1d789f9dc8ef708c399f1909694dee0fd6b058c6
B2ma <beatrice.terer@gmail.com>
2023-06-02 18:58:02 +0000

    Created 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+  * prints - prints a string
+  */
+void __attribute__((constructor))prints(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
