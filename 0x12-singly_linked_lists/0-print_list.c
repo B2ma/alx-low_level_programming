cff2d9e22e41ca293338015f8e910c0a6f561532
B2ma <beatrice.terer@gmail.com>
2023-06-02 13:37:05 +0000

    Amended 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,14 @@ size_t print_list(const list_t *h)
 {
 	size_t nodeCount = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%u] [%s]\n", h->len, h->str);
-			h = h->next;
-			nodeCount++;
-		}
+	if (!h->str)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] [%s]\n", h->len, h->str);
+		h = h->next;
+		nodeCount++;
 	}
 	return (nodeCount);
 }
