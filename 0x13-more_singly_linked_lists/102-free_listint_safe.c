777e8d4e23af8cdf2458323a0f2285fd05bb1a16
B2ma <beatrice.terer@gmail.com>
2023-06-05 20:19:51 +0000

    added the edge case when h and *h is null

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,8 +8,6 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *current = *h;
-	listint_t *slow = *h;
-	listint_t *fast = *h;
 	listint_t *tmp;
 	size_t count = 0;
 
@@ -17,33 +15,20 @@ size_t free_listint_safe(listint_t **h)
 		return (count);
 	while (current != NULL)
 	{
-		if (current->next <= current)
+		if (current->next >= current)
 		{
-			*h = NULL;
-			return (count);
+			tmp = current->next;
+			current->next = NULL;
+			current = tmp;
+			count++;
 		}
-		current = current->next;
-	}
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		else
 		{
-			*h = NULL;
-			return (count);
+			tmp = current;
+			current = current->next;
+			free(tmp);
+			count++;
 		}
-		tmp = slow;
-		slow = slow->next;
-		free(tmp);
-		count++;
-	}
-	while (*h != NULL)
-	{
-		tmp = *h;
-		*h = (*h)->next;
-		free(tmp);
-		count++;
 	}
 	*h = NULL;
 	return (count);
