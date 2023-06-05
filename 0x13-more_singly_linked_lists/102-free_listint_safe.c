838f8d1220735f3db60b3a99d2cbaa80e735dbae
B2ma <beatrice.terer@gmail.com>
2023-06-05 19:54:52 +0000

    Amended the edge case to check for loops and assign *h to NULL

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,22 +8,42 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *current = *h;
+	listint_t *slow = *h;
+	listint_t *fast = *h;
+	listint_t *tmp;
 	size_t count = 0;
 
 	if (h == NULL || *h == NULL)
-		return (0);
+		return (count);
 	while (current != NULL)
 	{
-		listint_t *tmp = current;
-
-		current = current->next;
-		free(tmp);
-		count++;
-		if (tmp <= current)
+		if (current->next <= current)
 		{
 			*h = NULL;
-			break;
+			return (0);
 		}
+		current = current->next;
+	}
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			*h = NULL;
+			return (count);
+		}
+		tmp = slow;
+		slow = slow->next;
+		free(tmp);
+		count++;
+	}
+	while (*h != NULL)
+	{
+		tmp = *h;
+		*h = (*h)->next;
+		free(tmp);
+		count++;
 	}
 	*h = NULL;
 	return (count);
