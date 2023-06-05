d38d8d2465f846d72ea93c5267bb0c757ee9e69e
B2ma <beatrice.terer@gmail.com>
2023-06-05 11:06:08 +0000

    Created 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+  * insert_nodeint_at_index - inserts a new node at a given position
+  * @head: pointer to head pointer
+  * @idx: index of the list where the new node should be added
+  * @n: data of the added node
+  * Return: address of the new node, or NULL if it failed
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *insertedNode = malloc(sizeof(listint_t));
+	unsigned int i = 0;
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		if (i == idx - 1)
+		{
+			if (insertedNode == NULL)
+				return (NULL);
+			insertedNode->n = n;
+			insertedNode->next = current->next;
+			current->next = insertedNode;
+			return (insertedNode);
+		}
+		current = current->next;
+		i++;
+	}
+	return (NULL);
+}
