c8e8303a5a7beb981c6c4aebf309cfd29439c48e
B2ma <beatrice.terer@gmail.com>
2023-06-05 19:12:22 +0000

    Added the edgecase when h or *h is NULL

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 
 /**
-  * free_listint_safe - frees a listint_t list
-  * @h: pointer to the list
-  * Return: the size of the list that was free’d
-  */
+    * free_listint_safe - frees a listint_t list
+    * @h: pointer to the list
+    * Return: the size of the list that was free’d
+    */
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *current = *h;
 	size_t count = 0;
 
+	if (h == NULL || *h == NULL)
+		return (0);
 	while (current != NULL)
 	{
 		listint_t *tmp = current;
