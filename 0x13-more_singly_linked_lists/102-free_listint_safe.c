d9e1c37c0d173f1ffc0a84aef6a72c94eef5feb1
B2ma <beatrice.terer@gmail.com>
2023-06-06 13:02:10 +0000

    Amended 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -6,16 +6,40 @@
     */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *tmp;
+	listint_t *hare, *tortoise, *tmp, *last;
 	size_t count = 0;
 
 	if (h == NULL || *h == NULL)
 		return (0);
+	tortoise = *h;
+	hare = *h;
+	last = NULL;
+	while (tortoise != NULL && tortoise->next != NULL)
+	{
+		hare = hare->next;
+		tortoise = tortoise->next->next;
+		if (hare == tortoise)
+		{
+			tortoise = *h;
+			while (tortoise != hare)
+			{
+				last = hare;
+				hare = hare->next;
+				tmp = tortoise->next;
+				free(tortoise);
+				count++;
+				tortoise = tmp;
+			}
+			last->next = NULL;
+			*h = NULL;
+			return (count);
+		}
+	}
 	while (*h != NULL)
 	{
-		current = *h;
+		tmp = *h;
 		*h = (*h)->next;
-		free(current);
+		free(tmp);
 		count++;
 	}
 	*h = NULL;
