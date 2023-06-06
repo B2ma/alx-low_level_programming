865c0285148c393b78b495f7033b1b3ded676fbc
B2ma <beatrice.terer@gmail.com>
2023-06-06 08:50:54 +0000

    Added the edgecase when head or head->next is null

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,6 +11,10 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *tortoise = head;
 	listint_t *hare = head;
 
+	if (head == NULL || head->next == NULL)
+	{
+		return (NULL);
+	}
 	while (hare != NULL && hare->next != NULL)
 	{
 		tortoise = tortoise->next;
