240f9a571053782d93350e7d2f0a83952b726d0e
B2ma <beatrice.terer@gmail.com>
2023-06-13 09:18:20 +0000

    Amended 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,10 +4,14 @@
   * error_print - prints error message to standard error
   * @sms: massage to print
   * @number: code to exit
+  * @filename: name of file
   */
-void error_print(const char *sms, int number)
+void error_print(const char *sms, const char *filename, int number)
 {
-	dprintf(STDERR_FILENO, "%s\n", sms);
+	if (filename != NULL)
+		dprintf(STDERR_FILENO, "%s %s\n", sms, filename);
+	else
+		dprintf(STDERR_FILENO, "%s\n", sms);
 	exit(number);
 }
 /**
@@ -23,13 +27,13 @@ int main(int argc, char **argv)
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		error_print("Usage: cp file_from file_to", 97);
+		error_print("Usage: cp file_from", "file_to", 97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_print("Error: Can't read from file", 98);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		error_print("Error: Can't read from file", argv[1], 98);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		error_print("Error: Can't write to file", 99);
+		error_print("Error: Can't write to file", argv[2], 99);
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -37,16 +41,22 @@ int main(int argc, char **argv)
 		{
 			close(fd_from);
 			close(fd_to);
-			error_print("Error: Can't write to file", 99);
+			error_print("Error: Can't write to file", argv[2], 99);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		close(fd_from);
 		close(fd_to);
-		error_print("Error: Can't read from file", 98);
+		error_print("Error: Can't read from file", argv[1], 98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-		error_print("Error: Can't close fd", 100);
+	if (close(fd_from) == -1)
+	{
+		error_print("Error: Can't close fd", "fd_from", 100);
+	}
+	else if (close(fd_to) == -1)
+	{
+		error_print("Error: Can't close fd", "fd_to", 100);
+	}
 	return (0);
 }
