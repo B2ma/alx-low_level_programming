95c99d2c683c2242af1e1c3309fc4d0d119601b2
B2ma <beatrice.terer@gmail.com>
2023-06-13 14:59:24 +0000

    Aligned the output

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -22,8 +22,7 @@ void print_elf_header(int fd)
 	unsigned char magic[16];
 	Elf64_Ehdr header;
 	off_t offset;
-	long unsigned int i;
-
+	unsigned long int i;
 	ssize_t bytes_read = read(fd, magic, sizeof(magic));
 
 	if (bytes_read == 1 || bytes_read < (ssize_t)sizeof(magic))
@@ -31,29 +30,34 @@ void print_elf_header(int fd)
 	if (magic[0] != 0x7f || magic[1] != 'E' || magic[2] != 'L' || magic[3] != 'F')
 		error_print ("Not and ELF file", NULL, 98);
 	offset = lseek(fd, ELF_HEADER_SIZE - sizeof(magic), SEEK_SET);
-
 	if (offset == -1)
 		error_print("Can't seek to  ELF header", NULL, 98);
 	bytes_read = read(fd, &header, sizeof(header));
 	if (bytes_read == -1 || bytes_read < (ssize_t)sizeof(header))
 		error_print("Can't read ELF headr", NULL, 98);
 	printf("ELF Header:\n");
-	printf("Magic:\t");
+	printf("  Magic:  ");
 	for (i = 0; i < sizeof(magic); i++)
 	{
 		printf("%02x ", magic[i]);
 	}
 	printf("\n");
-	printf(" Class:\t\t\t\t\t%s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf(" Data:\t\t\t\t\t%s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ?
+	printf("  Class:                             %s\n", header.e_ident[EI_CLASS]
+			== ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n", header.e_ident[EI_DATA] ==
+			ELFDATA2LSB ?
 			"2's complement, little endian" : "2's complement, big endian");
-	printf(" Version:\t\t\t\t%d (current)\n", header.e_ident[EI_VERSION]);
-	printf(" OS/ABI:\t\t\t\t%s\n", header.e_ident[EI_OSABI] ==
-			ELFOSABI_SYSV ? "UNIX - System V" : "Others");
-	printf(" ABI Version:\t\t\t\t%d\n", header.e_ident[EI_ABIVERSION]);
-	printf(" Type:\t\t\t\t\t%s\n", header.e_type == ET_EXEC ? "Executable file" :
+	printf("  Version:                           %d (current)\n", header.e_ident
+			[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n", header.e_ident[EI_OSABI]
+			== ELFOSABI_SYSV ? "UNIX - System V" : "Others");
+	printf("  ABI Version:                       %d\n", header.e_ident
+			[EI_ABIVERSION]);
+	printf("  Type:                              %s\n", header.e_type == ET_EXEC ?
+			"Executable file" :
 			(header.e_type == ET_DYN ? "Shared object file" : "Others"));
-	printf(" Entry point address:\t\t\t%#lx\n", (unsigned long)header.e_entry);
+	printf("  Entry point address:               %#lx\n", (unsigned long)
+			header.e_entry);
 }
 /**
   * main - Entry point
