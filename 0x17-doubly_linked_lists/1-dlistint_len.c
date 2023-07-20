94c57a176c74527279aada96fb47b8af9ef5cc0f
B2ma <beatrice.terer@gmail.com>
2023-07-20 09:35:22 +0000

    Created 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * dlistint_len -returns the number of elements in a linked dlistint_t list
+  * @h: pointer to head of double linked list
+  * Return: number of elements
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	const dlistint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		++i;
+	}
+	return (i);
+}
