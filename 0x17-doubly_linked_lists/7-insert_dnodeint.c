f1330d60ac7d22f25c9edfeccc33f051988d5a42
B2ma <beatrice.terer@gmail.com>
2023-07-20 16:56:21 +0000

    incorporated calling of other functions to ensure the function works at each node position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	if (idx == len)
+	{
+		return (add_dnodeint_end(h, n));
+	}
 	newNode = malloc(sizeof(dlistint_t));
 
 	if (newNode == NULL)
