b24297dfc857cfb1132ea29e5926b6f16d39bc7f
B2ma <beatrice.terer@gmail.com>
2023-07-23 16:31:21 +0000

    Added when i is equal or greater than index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,15 +27,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(nodeToDelete);
 		return (1);
 	}
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index - 1; i++)
 	{
-		if (nodeToDelete == NULL)
-		{
-			return (-1);
-		}
 		nodeToDelete = nodeToDelete->next;
 	}
-	if (nodeToDelete == NULL || nodeToDelete->next == NULL)
+	if (nodeToDelete == NULL || nodeToDelete->next == NULL || i >= index)
 	{
 		return (-1);
 	}
