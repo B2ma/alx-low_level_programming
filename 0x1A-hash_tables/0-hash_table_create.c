c9e4223bba315eba94238769233f11fa96073614
B2ma <beatrice.terer@gmail.com>
2023-08-11 19:01:41 +0000

    Updated 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,14 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	table = (hash_table_t *)malloc(sizeof(hash_table_t) +
-			(size * sizeof(hash_node_t *)));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
 	table->size = size;
-	table->array = (hash_node_t **)(table + 1);
+	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
