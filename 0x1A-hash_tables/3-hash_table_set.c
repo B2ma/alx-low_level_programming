4105d21fb8a0a8d9c6ea0ef3a6de673b413419ba
B2ma <beatrice.terer@gmail.com>
2023-08-11 15:54:18 +0000

    Updated 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,35 +10,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *newNode = NULL;
-	hash_node_t *current = NULL;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
-	current = ht->array[index];
-	while (current != NULL)
-	{
-		if (strcmp(current->key, key) == 0)
-		{
-			free(current->value);
-			current->value = strdup(value);
-			if (current->value == NULL)
-				return (0);
-			return (-1);
-		}
-		current = current->next;
-	}
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
 	newNode->key = strdup(key);
-	newNode->value = strdup(value);
-	if (newNode->key == NULL || newNode->value == NULL)
+	if (newNode->key == NULL)
 	{
-		free(newNode->key);
-		free(newNode->value);
 		free(newNode);
 		return (0);
+	}
+	newNode->value = (value != NULL) ? strdup(value) : strdup("");
+	if (newNode->value == NULL)
+	{
+		free(newNode->key);
+		free(newNode);
+		return 0;
 	}
 	newNode->next = ht->array[index];
 	ht->array[index] = newNode;
