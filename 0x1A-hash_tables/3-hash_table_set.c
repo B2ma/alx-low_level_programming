3c823e4ee470606aec3155c8539477db105eece2
B2ma <beatrice.terer@gmail.com>
2023-08-11 18:07:27 +0000

    Updated 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,8 +12,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newNode;
 	hash_node_t *current;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value), current->value = (value != NULL)
+				? strdup(value) : strdup("");
+		if (current->value == NULL)
+		{
+			return (0);
+		}
+		return (1);
+	}
+	current = current->next;
+	}
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
@@ -29,22 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(newNode->key), free(newNode);
 		return (0);
 	}
-	current = ht->array[index];
-	while (current != NULL)
-	{
-		if (strcmp(current->key, key) == 0)
-		{
-			free(current->value), current->value = (value != NULL)
-				? strdup(value) : strdup("");
-		if (current->value == NULL)
-		{
-			return (0);
-		}
-		free(newNode->key), free(newNode->value), free(newNode);
-		return (1);
-	}
-	current = current->next;
-	}
-	newNode->next = ht->array[index], ht->array[index] = newNode;
-	return 1;
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
+	return (1);
 }
