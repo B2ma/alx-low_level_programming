13dc3e635f4a88127b34cf42fb592a0779cebb81
B2ma <beatrice.terer@gmail.com>
2023-09-27 11:43:41 +0000

    corrected pycodestyle errors

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Island perimeter module"""
 
+
 def island_perimeter(grid):
     """"
     A function that returns the parameter of the island decribed in grid
@@ -15,13 +16,12 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for column in range(len(grid[row])):
             if grid[row][column] == 1:
-                if row == 0 or grid[row -1][column] == 0:
-                    perimeter +=1
-                if row == len(grid) - 1 or grid[row + 1][column] ==0:
-                    perimeter +=1
+                if row == 0 or grid[row - 1][column] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][column] == 0:
+                    perimeter += 1
                 if column == 0 or grid[row][column - 1] == 0:
                     perimeter += 1
                 if column == len(grid[row]) - 1 or grid[row][column + 1] == 0:
                     perimeter += 1
     return perimeter
-
